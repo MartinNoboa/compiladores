55b5c49942fc85787e819738c6f342caaf8c9b44
Martin <nomonoboa@gmail.com>
2023-04-22 19:26:48 -0600

    Add structure for first and follow methods

--- scripts/grammar/Grammar.py
+++ scripts/grammar/Grammar.py
@@ -8,10 +8,12 @@ class Grammar:
         self.nonTerminal = left
         self.terminal = []
         self.production = right
-        self.first = {}
+        self.firsts = {}
         self.follows = {}
         
     #declare methods
+    
+    # Terminals and Nonterminals ------------------------------------------------------------------------------
     def terminals(self):
         self.terminal = [char for char in self.production if char not in self.nonTerminal and char != 'eps']
     
@@ -21,7 +23,33 @@ class Grammar:
     def terminalsToString(self):
         print("Terminal:", ', '.join(self.terminal))
     
-    
     def nonTerminalsToString(self):
         print("Terminal:", ', '.join(self.nonTerminal))
 
+    # Grammar---------------------------------------------------------------------------------------------------
+    def grammarToString(self):
+        pass
+    
+    # First and Follows -----------------------------------------------------------------------------------------
+    def setFirst(self, key, values):
+        # add key to First dictionary and set set of values
+        pass
+    
+    def setFollow(self, key, values):
+        # add key to First dictionary and set set of values
+        pass
+    
+    def first(self, key):
+        return self.firsts[key] 
+    
+    def follow(self, key):
+        return self.follows[key] 
+    
+    def getFirsts(self):
+        return self.firsts
+    
+    def getFollows(self):
+        return self.follows
+    
+    def hasNext(self, index, value):
+        pass
